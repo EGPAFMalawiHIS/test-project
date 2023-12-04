3c43ee01053585b44cd63f9c73bbdcadcce3c01d
Roy Chanunkha <roychanunkha@gmail.com>
2023-12-04 11:24:00 +0200

    adding new http methods

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
+app.put("/", (req, res) => {
+  res.send("Got a PUT request");
+});
+
+app.delete("/", (req, res) => {
+  res.send("Got a DELETE request");
 });
 
 app.listen(port, () => {
